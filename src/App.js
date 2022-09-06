f72418305ac3268f2fa1f8cfdd3e2f53e82cd4a4
Utkarsh Gupta <500075374@stu.upes.ac.in>
2022-09-07 00:41:22 +0530

    SEO + Theme + Logo + Icon
    Completed SEO for the page, decided and applied color theme for the page, designed and added logo and favicon to the page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.sass";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useState("dark");
+
+	return (
+		<div className="App">
+			<div className={theme}>
+				<h1>Hello World!</h1>
+				<h2>Hello World!</h2>
+				<h3>Hello World!</h3>
+				<h4>Hello World!</h4>
+				<h5>Hello World!</h5>
+				<h6>Hello World!</h6>
+				<p>Hello World!</p>
+				<button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+					Change Theme
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
